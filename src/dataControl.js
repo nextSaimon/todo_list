3a2d2c832480b028a0de86e23fa49256a94fa35a
Saimon <saimonss5432@gmail.com>
2025-09-01 09:14:45 +0600

    ok

--- src/dataControl.js
+++ src/dataControl.js
@@ -1,20 +1,24 @@
 import { createWebClient } from "./lib/appwriteWeb";
-
+import { cookies } from "next/headers";
+import fs from "fs";
+import { ID, Permission, Role, Query } from "appwrite";
 const getData = {
-
-}
-const postData = {
   toDoList: async () => {
-    
-    const {tablesDB}= createWebClient()
-  }
-  
-}
-const putData = {
-  
-}
+    const jwtToken = (await cookies()).get("jwt").value;
+    try {
+      const { tablesDB } = createWebClient(jwtToken);
+      const data = await tablesDB.listRows("db1", "todolist");
+      console.log(data);
+      // // save data in json
+      const jsonData = JSON.stringify(data.rows, null, 2);
+      return data;
+    } catch (error) {
+      console.log("error in toDoList..", error);
+    }
+  },
+};
+const postData = {};
+const putData = {};
 
-const deleteData = {
-  
-}
-export {getData, postData, putData, deleteData}
+const deleteData = {};
+export { getData, postData, putData, deleteData };
